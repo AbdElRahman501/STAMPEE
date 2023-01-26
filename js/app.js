fcde970833b09f8fe4a3d4948bb70d3c46e83152
bedo <bedo.ahmed416@gmail.com>
2023-01-26 14:48:46 +0200

    add actions and edit cards

--- js/app.js
+++ js/app.js
@@ -5,46 +5,57 @@ function scrollToTop() {
     behavior: 'smooth'
   })
 }
-const slider = document.querySelector(".scroll-container")
-let x = slider.offsetWidth
-let y = slider.scrollLeft
-const pv = slider.querySelector(".previous")
-const nx = slider.querySelector(".next")
+const sliders = document.querySelectorAll(".scroll-container")
 
-const cards = slider.querySelectorAll(".card")
+sliders.forEach(slider => {
+  let x = slider.offsetWidth
+  let y = slider.scrollLeft
+  let pv = slider.querySelector(".previous")
+  let nx = slider.querySelector(".next")
 
-function scroller() {
-  x = slider.offsetWidth
-  y = slider.scrollLeft
-  console.log(y);
-  if (y < 10) {
-    pv.setAttribute("style", "display:none;")
-  } else {
-    pv.setAttribute("style", "")
+  let cards = slider.querySelectorAll(".card")
+
+  function scroller() {
+    x = slider.offsetWidth
+    y = slider.scrollLeft
+    if (y < 10) {
+      pv.setAttribute("style", "display:none;")
+    } else {
+      pv.setAttribute("style", "")
+    }
+    if (x > cards[cards.length - 1].getBoundingClientRect().x) {
+      nx.setAttribute("style", "display:none;")
+    } else {
+      nx.setAttribute("style", "")
+    }
   }
-  if (x > cards[cards.length - 1].getBoundingClientRect().x) {
-    nx.setAttribute("style", "display:none;")
-  } else {
-    nx.setAttribute("style", "")
+
+  slider.addEventListener('scroll', scroller)
+  scroller()
+  let pvBtn = slider.querySelector(".previous button")
+  let nxBtn = slider.querySelector(".next button")
+
+  pvBtn.addEventListener('click', scrollLift)
+  nxBtn.addEventListener('click', scrollRight)
+
+
+  function scrollRight() {
+    x = x + y
+    slider.scrollTo({
+      left: x,
+      top: 0,
+      behavior: 'smooth'
+    })
   }
-}
+  function scrollLift() {
+    x = y - x
+    slider.scrollTo({
+      left: x,
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+});
 
-slider.addEventListener('scroll', scroller)
-scroller()
 
-function scrollRight() {
-  x = x + y
-  slider.scrollTo({
-    left: x,
-    top: 0,
-    behavior: 'smooth'
-  })
-}
-function scrollLift() {
-  x = y - x
-  slider.scrollTo({
-    left: x,
-    top: 0,
-    behavior: 'smooth'
-  })
-}
+
