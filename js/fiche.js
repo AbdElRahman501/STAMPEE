46b60c79332d71165c9e8472320834834cc4162e
bedo <bedo.ahmed416@gmail.com>
2023-01-30 03:41:50 +0200

    add more clear stamps and slide effect in fiche

--- js/fiche.js
+++ js/fiche.js
@@ -1,8 +1,8 @@
 const itemsNav = document.querySelector('.items-nav')
 const navSlider = document.querySelectorAll(".slider-nav")
 const checkBoxes = itemsNav.querySelectorAll('[type="checkbox"]');
-
-
+const imageSlider = document.querySelector(".main-image")
+const imageSwitcher = document.querySelector(".sub-image")
 
 checkBoxes.forEach(function (checkBox) {
     checkBox.addEventListener("click", function (event) {
@@ -30,7 +30,7 @@ navSlider.forEach(slider => {
             pv.setAttribute("style", "")
             nx.setAttribute("style", "display:none;")
         }
-       
+
     }
 
     slider.addEventListener('scroll', scroller)
@@ -57,3 +57,44 @@ navSlider.forEach(slider => {
         })
     }
 });
+
+function slideImage() {
+    let images = imageSlider.querySelectorAll("img")
+    let subImages = []
+    imageSlider.addEventListener("scroll", function (event) {
+        let target = Number((event.target.scrollLeft / images[0].offsetWidth).toFixed(0))
+        activeImage(target)
+
+    })
+    function activeImage(target) {
+        for (let i = 0; i < subImages.length; i++) {
+            const image = subImages[i];
+            if (i === target) {
+                image.classList.add("active")
+            } else {
+                image.classList.remove("active")
+            }
+
+        }
+    }
+    for (let i = 0; i < images.length; i++) {
+        const image = images[i].cloneNode(true);
+        image.setAttribute("name", i)
+
+        image.addEventListener("click", function (event) {
+            console.log(event.target.name, images[i].offsetLeft, imageSlider.offsetLeft);
+            let x = images[i].offsetLeft - imageSlider.offsetLeft
+            imageSlider.scrollTo({
+                left: x,
+                top: 0,
+                behavior: 'smooth'
+            })
+
+        })
+
+        imageSwitcher.appendChild(image)
+        subImages.push(image)
+    }
+    activeImage(0)
+}
+slideImage()
